95c96f0dd1d907c1a704a13c3a04bf99563509cb
Arman Ali Khan <armaanalikhanofficial@gmail.com>
2026-04-23 14:26:24 +0000

    move firebase config on .env file.

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,16 +3,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 export const firebaseConfig: FirebaseOptions = {
-    // THIS IS A PLACEHOLDER.
-    // Go to your Firebase project console, find "Project settings" > "General",
-    // and under "Your apps", create a new Web app or select an existing one.
-    // Then, copy the firebaseConfig object and paste it here.
-    apiKey: "AIzaSyDMifaZye74NLSfeqSlHYzfDnbnhNsTCJg",
-  authDomain: "url-shortner-69404.firebaseapp.com",
-  projectId: "url-shortner-69404",
-  storageBucket: "url-shortner-69404.firebasestorage.app",
-  messagingSenderId: "1086705164994",
-  appId: "1:1086705164994:web:133f006d04437de503d15b"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
